a4545435ccaa8b5c8ea94525682fda220d83d9b4
Myrian_Fernanda <myrian.fernanda@academico.ufpb.br>
2023-10-03 23:04:55 -0300

    adição de token e permissões

--- ultima/rest_api/views.py
+++ ultima/rest_api/views.py
@@ -4,15 +4,20 @@ from django.shortcuts import render
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet #adicionar viewset
 
-from reserva.models import Reserva
+from reserva.models import Reserva #Referenciar modelo
 from rest_api.serializers import AgendamentoModelSerializer
 
+
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly #permissão apenas para leitura
 
 class AgendamentoModelViewSet(ModelViewSet):
       queryset = Reserva.objects.all()
       serializer_class = AgendamentoModelSerializer
+      authentication_classes = [TokenAuthentication]
+      permission_classes = [IsAuthenticatedOrReadOnly]
       
 
 #essa função sera acessada atrasves dos métodos get e post
--- ultima/ultima/settings.py
+++ ultima/ultima/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'reserva',
     'rest_framework',
     'rest_api',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
